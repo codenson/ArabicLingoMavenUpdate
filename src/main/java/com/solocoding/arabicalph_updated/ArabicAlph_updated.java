b354cf38e272c1688cb1756d1eee822e80071ed8
guero <Marouane Guerouji>
2023-08-19 19:14:33 -0400

    Added Main Menu. Added container to hold panels and the main JFrame to set up different views.

---
+++ src/main/java/com/solocoding/arabicalph_updated/ArabicAlph_updated.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.solocoding.arabicalph_updated;
+
+/**
+ *
+ * @author guero
+ */
+public class ArabicAlph_updated {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
